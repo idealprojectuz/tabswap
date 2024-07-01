13dc0a050014af1c1c758c11837d84a341a97e94
Hayotbek <idealprojectuz@gmail.com>
2024-07-01 16:54:35 +0500

    update

--- humster.js
+++ humster.js
@@ -11,9 +11,9 @@ const humster = async (token) => {
       maxBodyLength: Infinity,
       url: "https://api.hamsterkombat.io/clicker/tap",
       headers: {
+        Authorization: "Bearer " + token,
         Host: "api.hamsterkombat.io",
         Accept: "application/json",
-        Authorization: "Bearer " + token,
         "Sec-Fetch-Site": "same-site",
         "Accept-Language": "uz,en-GB;q=0.9,en;q=0.8",
         "Sec-Fetch-Mode": "cors",
@@ -42,7 +42,109 @@ const humster = async (token) => {
     // getCoin();
   }
 };
-module.exports = humster;
+
+const hamsterSync = async (token) => {
+  try {
+    let config = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: "https://api.hamsterkombat.io/clicker/sync",
+      headers: {
+        accept: "*/*",
+        "accept-language": "en-US,en;q=0.9,uz;q=0.8",
+        authorization: "Bearer " + token,
+        "cache-control": "no-cache",
+        pragma: "no-cache",
+      },
+    };
+
+    const response = await axios(config);
+    if (response.status == 200 || response.status == 201) {
+      const { boosts, upgrades, tasks, airdropTasks, referral, ...result } =
+        response.data?.clickerUser;
+      // const result = await response.data?.clickerUser;
+
+      return result;
+    }
+    // console.log(response.data);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const hamsterFullEnergy = async (token) => {
+  try {
+    const config = {
+      url: "https://api.hamsterkombat.io/clicker/buy-boost",
+      headers: {
+        Host: "api.hamsterkombat.io",
+        Accept: "application/json",
+        "Sec-Fetch-Site": "same-site",
+        "Accept-Language": "uz,en-GB;q=0.9,en;q=0.8",
+        "Sec-Fetch-Mode": "cors",
+        Origin: "https://hamsterkombat.io",
+        "User-Agent":
+          "Mozilla/5.0 (iPhone; CPU iPhone OS 17_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148",
+        Referer: "https://hamsterkombat.io/",
+        Connection: "keep-alive",
+        "Sec-Fetch-Dest": "empty",
+        Authorization: "Bearer " + token,
+
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      data: {
+        boostId: "BoostFullAvailableTaps",
+        timestamp: Date.now(),
+      },
+    };
+    const response = await axios(config);
+    if (response.status == 200 || response.status == 201) {
+      const { upgrades, tasks, airdropTasks, referral, ...result } =
+        response.data?.clickerUser;
+      return result;
+    }
+  } catch (error) {
+    console.log(error.response.data);
+    return error.response.data;
+  }
+};
+
+const dailybonus = async (token) => {
+  try {
+    const config = {
+      url: "https://api.hamsterkombat.io/clicker/check-task",
+      headers: {
+        accept: "application/json",
+        "accept-language": "en-US,en;q=0.9",
+        authorization: "Bearer " + token,
+        "cache-control": "no-cache",
+        "content-type": "application/json",
+        pragma: "no-cache",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-site",
+        Referer: "https://hamsterkombat.io/",
+        "Referrer-Policy": "strict-origin-when-cross-origin",
+      },
+      method: "POST",
+      data: {
+        taskId: "streak_days",
+      },
+    };
+    const response = await axios(config);
+    if (response.status == 200 || response.status == 201) {
+      const { task } = response.data;
+      return task;
+    }
+  } catch (error) {
+    console.log(error.message);
+    console.log(error.response.data);
+    return error.response.data;
+  }
+};
+
+module.exports = { humster, hamsterSync, hamsterFullEnergy, dailybonus };
 // getCoin().then((data) => console.log(data));
 // getCoin().then((data) => console.log(data));
 
--- index.js
+++ index.js
@@ -1,109 +1,30 @@
 require("dotenv").config();
-const humster = require("./humster");
+const {
+  humster,
+  hamsterSync,
+  hamsterFullEnergy,
+  dailybonus,
+} = require("./humster");
 const pixel = require("./pixel");
 const os = require("os");
+const { Telegraf } = require("telegraf");
 // const axios = require("axios");
 const express = require("express");
-// const getAuth = async () => {
-//   try {
-//     let data = JSON.stringify({
-//       init_data:
-//         "query_id=AAF6AGw2AAAAAHoAbDa_04WQ&user=%7B%22id%22%3A913047674%2C%22first_name%22%3A%22Hayotbek%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22hayotbek_0427%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1716122262&hash=20a0e3b91ec1721a4254c19bff85d0218065eb9f2316af1ccffbdeac4d6cbbb1",
-//       referrer: "",
-//       bot_key: "app_bot_0",
-//     });
-
-//     let config = {
-//       method: "post",
-//       maxBodyLength: Infinity,
-//       url: "https://api.tapswap.ai/api/account/login",
-//       headers: {
-//         Host: "api.tapswap.ai",
-//         "x-cv": "1",
-//         Accept: "*/*",
-//         "Sec-Fetch-Site": "cross-site",
-//         "Accept-Language": "uz,en-GB;q=0.9,en;q=0.8",
-//         "Sec-Fetch-Mode": "cors",
-//         Origin: "https://app.tapswap.club",
-//         "x-app": "tapswap_server",
-//         "User-Agent":
-//           "Mozilla/5.0 (iPhone; CPU iPhone OS 17_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148",
-//         Referer: "https://app.tapswap.club/",
-//         Connection: "keep-alive",
-//         "Sec-Fetch-Dest": "empty",
-//         "Content-Type": "application/json",
-//       },
-//       data: data,
-//     };
-//     const response = await axios(config);
-//     return response.data.access_token;
-//   } catch (error) {
-//     console.log(error);
-//     throw new Error("Xatolik yuz berdi");
-//   }
-// };
-// const getCoin = async (token = null) => {
-//   try {
-//     let data = {
-//       taps: 5000,
-//       time: Date.now(),
-//     };
-//     let config = {
-//       method: "post",
-//       maxBodyLength: Infinity,
-//       url: "https://api.tapswap.ai/api/player/submit_taps",
-//       headers: {
-//         Host: "api.tapswap.ai",
-//         "x-cv": "1",
-//         Accept: "*/*",
-//         Authorization: token
-//           ? "Bearer " + token
-//           : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjkxMzA0NzY3NCwiaWF0IjoxNzE2MTIxNjMwLCJleHAiOjE3MTYxMjUyMzB9.QGuUX6iL5o2vgTrRmtCSluZUDV6cTvVtxDS6HHmObLo",
-//         "Content-Id": "912974832",
-//         "Sec-Fetch-Site": "cross-site",
-//         "Accept-Language": "uz,en-GB;q=0.9,en;q=0.8",
-//         "Sec-Fetch-Mode": "cors",
-//         Origin: "https://app.tapswap.club",
-//         "User-Agent":
-//           "Mozilla/5.0 (iPhone; CPU iPhone OS 17_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148",
-//         Referer: "https://app.tapswap.club/",
-//         "x-app": "tapswap_server",
-//         Connection: "keep-alive",
-//         "Sec-Fetch-Dest": "empty",
-//         "Content-Type": "application/json",
-//       },
-//       data: data,
-//     };
-
-//     const response = await axios(config);
-
-//     console.log("\n\nSuccess Cleamed");
-//     console.log("profile", response?.data?.player?.full_name);
-//     console.log("total tabs", response?.data?.player?.shares);
-//     // console.log(response.data);
-//     return response.data;
-//   } catch (error) {
-//     if (error?.response?.data?.message == "Unauthorized") {
-//       const newToken = await getAuth();
-//       if (newToken) {
-//         return getCoin(newToken);
-//       }
-//     }
-//     return getCoin();
-//     // console.log(error.message);
-//   }
-// };
-// // getCoin();
-
-// const interval = setInterval(() => {
-//   try {
-//     getCoin();
-//   } catch (error) {
-//     getCoin();
-//   }
-// }, 10000);
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
 const app = express();
+
+app.use(express.json());
+
+//webhook
+// app.post("/webhook", async (req, res) => {
+//   bot.handleUpdate(req.body, res);
+// });
+// bot.telegram.setWebhook(process.env.WEBHOOK);
+
+app.get("/env", async (req, res) => {
+  return res.json(process.env);
+});
 
 app.get("/", async (req, res) => {
   const result = await Promise.all([
@@ -124,7 +45,73 @@ app.get("/", async (req, res) => {
   res.writeHead(200, "ok", { "Content-Type": "application/json" });
   return res.end(JSON.stringify(result, null, 2));
 });
+
+//every one hour
+app.get("/sync", async (_, res) => {
+  const result = await Promise.all([
+    hamsterSync(process.env.TOKEN_HAYOTBEK),
+    hamsterSync(process.env.TOKEN_NODIRA),
+    hamsterSync(process.env.TOKEN_SAMANDAR),
+    hamsterSync(process.env.TOKEN_THAILAND),
+    hamsterSync(process.env.TOKEN_HAYOTBEK_OLD),
+    hamsterSync(process.env.TOKEN_JAVOHIR),
+    hamsterSync(process.env.TOKEN_AKOBIR),
+    hamsterSync(process.env.TOKEN_AKOBIR_MOTHER),
+    hamsterSync(process.env.TOKEN_AKOBIR_MOTHER_PHONE_2),
+    hamsterSync(process.env.TOKEN_ISLOM),
+    hamsterSync(process.env.TOKEN),
+    hamsterSync(process.env.TOKEN_NARGIZA),
+  ]);
+  return res.json(result);
+});
+
+//every one hour
+app.get("/fullenergy", async (_, res) => {
+  const result = await Promise.all([
+    hamsterFullEnergy(process.env.TOKEN_HAYOTBEK),
+    hamsterFullEnergy(process.env.TOKEN_NODIRA),
+    hamsterFullEnergy(process.env.TOKEN_SAMANDAR),
+    hamsterFullEnergy(process.env.TOKEN_THAILAND),
+    hamsterFullEnergy(process.env.TOKEN_HAYOTBEK_OLD),
+    hamsterFullEnergy(process.env.TOKEN_JAVOHIR),
+    hamsterFullEnergy(process.env.TOKEN_AKOBIR),
+    hamsterFullEnergy(process.env.TOKEN_AKOBIR_MOTHER),
+    hamsterFullEnergy(process.env.TOKEN_AKOBIR_MOTHER_PHONE_2),
+    hamsterFullEnergy(process.env.TOKEN_ISLOM),
+    hamsterFullEnergy(process.env.TOKEN),
+    hamsterFullEnergy(process.env.TOKEN_NARGIZA),
+  ]);
+  return res.json(result);
+});
+// app.get('/')
+
+//every  day
+app.get("/dailybonus", async (_, res) => {
+  const result = await Promise.all([
+    dailybonus(process.env.TOKEN_HAYOTBEK),
+    dailybonus(process.env.TOKEN_NODIRA),
+    dailybonus(process.env.TOKEN_SAMANDAR),
+    dailybonus(process.env.TOKEN_THAILAND),
+    dailybonus(process.env.TOKEN_HAYOTBEK_OLD),
+    dailybonus(process.env.TOKEN_JAVOHIR),
+    dailybonus(process.env.TOKEN_AKOBIR),
+    dailybonus(process.env.TOKEN_AKOBIR_MOTHER),
+    dailybonus(process.env.TOKEN_AKOBIR_MOTHER_PHONE_2),
+    dailybonus(process.env.TOKEN_ISLOM),
+    dailybonus(process.env.TOKEN),
+    dailybonus(process.env.TOKEN_NARGIZA),
+  ]);
+  return res.json(result);
+});
+
+app.get("/maxfiysoz", async (req, res) => {});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+// bot.start((ctx) => ctx.reply("Welcome!"));
+// bot.help((ctx) => ctx.reply("Send me a sticker"));
+// bot.on("sticker", (ctx) => ctx.reply("👍"));
+// bot.hears("hi", (ctx) => ctx.reply("Hey there"));
